175d9110e21319f004815b564c896bb573eaceaa
Yoyeo98137 <1695782408@qq.com>
2022-04-12 23:28:55 +0800

    update useRequest - 参数泛型推断补充、字段调整

--- src/hooks/useRequest/index.ts
+++ src/hooks/useRequest/index.ts
@@ -1,28 +1,14 @@
-import { onMounted, Ref, ref } from "vue"
-
-interface RequestResult<TRequset, TParams extends unknown[]> {
-  loading: any,
-  data: any,
-
-  run: any
-}
-interface PropsRequestOptions {
-  /** 接收发起自动请求时需要携带的参数 */
-  defaultParams?: any
-
-  /** 手动请求开关 */
-  manual?: boolean
-  /** 通过设置延迟的毫秒数，可以延迟 loading 变成 true 的时间，有效防止闪烁。 */
-  loadingDelay?: number
-}
+import { onMounted, ref } from "vue"
+import { Service, Options, RequestResult } from "../types"
 
 /**
  * *useRequest*
  * 
  * - [x] 自动请求/手动请求（兼容带参函数的正常调用）
  * - [x] loading delay
+ * - [x] 继承请求体入参类型推断 
+ * - [x] 泛型推断
  * - [ ] 重新加载（reload），这类似于再手动触发一下请求，但是从语义化，或者是参数不变的情况下，reload 是更匹配当前场景的
- * - [ ] 泛型推断
  * - [ ] 放置回调钩子（before、success、finally...）
  * - [ ] 应该自带 节流 能力
  * - [ ] ...
@@ -36,73 +22,83 @@ interface PropsRequestOptions {
  * 
  * todo types.
  */
-export function useRequest<TRequset, TParams extends unknown[] = any>(
-  service: any,
+export function useRequest<TRequset, TParams extends unknown[]>(
+  service: Service<TRequset, TParams>
 ): RequestResult<TRequset, TParams>;
-export function useRequest<TRequset, TParams extends unknown[] = any>(
-  service: any,
-  options: PropsRequestOptions
+export function useRequest<TRequset, TParams extends unknown[]>(
+  service: Service<TRequset, TParams>,
+  options: Options<TRequset, TParams>
 ): RequestResult<TRequset, TParams>;
-export function useRequest<TRequset, TParams>(service: any, options?: PropsRequestOptions) {
+export function useRequest<TRequset, TParams extends unknown[]>(
+  service: Service<TRequset, TParams>,
+  options?: Options<TRequset, TParams>
+) {
+
   const loading = ref(false)
+  const error = ref()
   const data = ref()
   const delayLoadingTimer = ref()
 
-  // todo 怎样可以通过解构的形式，同时又保证 ts 的正常推断
-  // 可以再封装一层函数
-  // 这里的 options 是非必填的，然后加层的函数里面则保证一个默认值 空对象
-  // 这样就可以对其进行解构了
-  // return useRequestImplement<TRequset, TParams>(service, options);
-  // const { manual = false } = options
+  // todo 解构尝试
 
   /** 是否开启手动请求开关 */
-  const isManualRun = () => options?.manual
+  const isManualRun = options?.manual
   /** 是否开启延迟加载 */
-  const isLoadingDelay = () => options?.loadingDelay
-
+  const isLoadingDelay = options?.loadingDelay
+  /** 统一清理定时器 */
   const clearTimerAll = () => {
     delayLoadingTimer.value && delayLoadingTimer.value()
   }
 
-  const _run = (args: any) => {
+  const _run = (args: TParams) => {
+    // Clear all
     clearTimerAll()
 
-    loading.value = !isLoadingDelay()
+    loading.value = !isLoadingDelay
     delayLoadingTimer.value = checkDelayLoading()
 
     service(...args)
-      .then((res: any) => {
+      .then((res) => {
+        console.log('🏄 # service......')
         console.log('🏄 # service # res', res)
+        console.log('🏄 # service......end')
+        console.log('')
 
         data.value = res
-        loading.value = false
-      }).catch((error: any) => {
-        // 
+        if (options?.onSuccess) options?.onSuccess(res, args)
+      }).catch((err) => {
+        error.value = err
+
       }).finally(() => {
         // clear Delay timer
         delayLoadingTimer.value()
+
+        // init loading
+        loading.value = false
       })
   }
   // *手动请求
-  const run = (...args: any) => {
-    // isManualRun() && _run(args)
+  const run = (...args: TParams) => {
+    // isManualRun && _run(args)
     // manual 的用意应该是 静止自动发起请求，而不是跟 run 回调捆绑的关系
     _run(args)
   }
   // *自动请求
   onMounted(() => {
+    if (isManualRun) return
+
     // 参数兼容
     const params = options?.defaultParams?.length ? options?.defaultParams : []
-    !isManualRun() && _run(params)
+    _run(params as TParams)
   })
 
   // *检测是否开启加载延迟
   const checkDelayLoading = () => {
     let delayTimer: number
-    if (isLoadingDelay()) {
+    if (isLoadingDelay) {
       delayTimer = setTimeout(() => {
         loading.value = true
-      }, isLoadingDelay());
+      }, isLoadingDelay);
     }
 
     return () => delayTimer && clearTimeout(delayTimer)
@@ -110,6 +106,7 @@ export function useRequest<TRequset, TParams>(service: any, options?: PropsReque
 
   return {
     loading,
+    error,
     data,
     run
   }
