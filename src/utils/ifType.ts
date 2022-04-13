d6dc531d47bc2534c1d2825aff370c0e65f30c5d
Yoyeo98137 <1695782408@qq.com>
2022-04-13 22:12:06 +0800

    update ifType

--- src/utils/ifType.ts
+++ src/utils/ifType.ts
@@ -1,3 +1,55 @@
-export const isFunction = (type: any) => {
-  // todo...
+
+// 变量类型判断
+
+export function isTypeOf(val: any, type: string) {
+  const oToString = Object.prototype.toString.call(val) as any
+  const curentType = oToString.match(/\s+(\w+)/)[1]
+
+  return curentType.toLowerCase() === type.toLowerCase()
+}
+
+export function isNull(s: any) {
+  return isTypeOf(s, "null")
+}
+
+export function isUndefined(s: any) {
+  return isTypeOf(s, "undefined")
+}
+
+export function isString(s: any) {
+  return isTypeOf(s, "string")
+}
+
+export function isBoolean(s: any) {
+  return isTypeOf(s, "boolean")
+}
+
+export function isNumber(s: any) {
+  return isTypeOf(s, "number")
+}
+
+export function isArray(s: any) {
+  return isTypeOf(s, "array")
+}
+
+export function isObject(s: any) {
+  return isTypeOf(s, "object")
+}
+
+export function isFunction(s: any) {
+  return isTypeOf(s, "function")
+}
+
+/** 是否为 Async 注册函数 */
+export function isAsyncFunction(s: any) {
+  return isTypeOf(s, "AsyncFunction")
+}
+
+/** 是否为空字符串 */
+export function emptyString(s: any) {
+  return isTypeOf(s, "string") && s.length === 0
+}
+
+export function notEmptyObject(s: any) {
+  return isTypeOf(s, "object") && Object.keys(s).length !== 0
 }
