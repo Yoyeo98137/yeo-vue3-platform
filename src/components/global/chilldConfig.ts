99a7d89a5bd67122e7046418f9aaec236323d6c3
Yoyeo98137 <1695782408@qq.com>
2022-05-28 20:05:42 +0800

    coding: 动态组件配置定义

---
+++ src/components/global/chilldConfig.ts
@@ -0,0 +1,11 @@
+import { ElInput } from 'element-plus';
+
+export const chilldConfig = {
+  input: {
+    component: ElInput,
+    baseAttrs: {
+      clearable: true,
+      // ...
+    },
+  },
+};
