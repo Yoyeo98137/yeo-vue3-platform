3388547157d46343f8f122377ed0a7446a7d8cfd
Yoyeo98137 <1695782408@qq.com>
2022-06-05 19:37:29 +0800

    update: component Radio、Select

---
+++ src/components/global/formConfig/YDydRadio.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { useSlots } from 'vue';
+import { notEmptyObject } from '@/utils/ifType';
+
+const slots: any = useSlots();
+console.log('🏄 # slots', slots);
+</script>
+
+<template>
+  <ElRadioGroup v-bind="$attrs">
+    <!-- todo -->
+    <!-- {{ $slots }} -->
+
+    <template v-for="(fRadio, fIndex) in $attrs.options" :key="fIndex">
+      <ElRadio :label="fRadio.value">
+        <!-- 当定义了自定义插槽名，且传入了插槽节点时 -->
+        <template v-if="fRadio.slotName && notEmptyObject(slots)">
+          <slot :name="fRadio.slotName"></slot>
+        </template>
+        <template v-else>{{ fRadio.label }}</template>
+      </ElRadio>
+    </template>
+  </ElRadioGroup>
+</template>
+
+<style scoped>
+/*  */
+</style>
---
+++ src/components/global/formConfig/YDydSelect.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+//
+</script>
+
+<template>
+  <ElSelect v-bind="$attrs">
+    <template v-for="fSelect in ($attrs.options as any)" :key="fSelect.value">
+      <ElOption :label="fSelect.label" :value="fSelect.value"></ElOption>
+    </template>
+  </ElSelect>
+</template>
+
+<style scoped>
+/*  */
+</style>
