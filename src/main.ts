369991d8525bff9d035d29da69885ab53f3616be
Yoyeo98137 <1695782408@qq.com>
2022-06-16 22:04:39 +0800

    update: elm-plus 导入样式缺失

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './router'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import App from './App.vue';
+
+// 手动导入样式
+import 'element-plus/es/components/loading/style/index';
+import 'element-plus/es/components/message/style/index';
+import 'element-plus/es/components/message-box/style/index';
+import 'element-plus/theme-chalk/src/index.scss';
 
 const app = createApp(App);
 
-app.use(router).use(createPinia()).mount("#app");
+app.use(router).use(createPinia()).mount('#app');
