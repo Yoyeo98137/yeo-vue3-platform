f0f4cc96b33ac98e28f252f433ab7f4f1cb80b86
Yoyeo98137 <1695782408@qq.com>
2022-06-18 22:44:07 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // 自动导入
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,24 +32,24 @@ export default defineConfig({
               if (atRule.name === 'charset') {
                 atRule.remove();
               }
-            }
-          }
-        }
-      ]
-    }
+            },
+          },
+        },
+      ],
+    },
   },
 
   server: {
     // #server.host
     // 指定服务器应该监听哪个 IP 地址。
     // 如果将此设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址。
-    host: "0.0.0.0",
-    port: 8182
+    host: '0.0.0.0',
+    port: 8182,
   },
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 
@@ -68,7 +68,7 @@ export default defineConfig({
       //   "element-plus/es",
       //   "axios",
       //   "dayjs",
-      external: ["vue", "element-plus/icons-vue"],
+      external: ['vue', 'element-plus/icons-vue'],
     },
-  }
-})
+  },
+});
