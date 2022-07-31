04e42ed3a9736eba1e00bbeb49dbe2ef52edea2f
Yoyeo98137 <1695782408@qq.com>
2022-07-31 17:55:40 +0800

    coding Cascader 声明注入类型

--- src/components/global/cascader/src/types.ts
+++ src/components/global/cascader/src/types.ts
@@ -1,6 +1,22 @@
+import { InjectionKey } from 'vue';
+import type { default as CascaderNode } from './node';
+
 export interface CascaderProps {
   lazy: boolean;
   lazyLoad: () => void;
 }
 export type CascaderConfig = Required<CascaderProps>;
 
+export interface CascaderPanelContext {
+  expandingNode: CascaderNode;
+  expandNode: (node: CascaderNode) => void;
+  handleCheckChange: (
+    node: CascaderNode,
+    checked: boolean,
+    // 触发选中 "submit" 通知路径确认（关闭弹窗）
+    emitClose?: boolean
+  ) => void;
+}
+
+export const CASCADER_PANEL_INJECTION_KEY: InjectionKey<CascaderPanelContext> =
+  Symbol();
