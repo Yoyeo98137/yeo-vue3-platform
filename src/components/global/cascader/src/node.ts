951a202d18daf4a8822d9f667652f7c60c3c2a1d
Yoyeo98137 <1695782408@qq.com>
2022-07-31 17:55:01 +0800

    coding Cascader 节点类定义、组件定义

--- src/components/global/cascader/src/node.ts
+++ src/components/global/cascader/src/node.ts
@@ -1,4 +1,6 @@
+import { isArray } from '@/utils/ifType';
 import type { CascaderConfig } from './types';
+import { Nullable } from './utils';
 
 export type CascaderNodeValue = string | number;
 export type CascaderNodePathValue = CascaderNodeValue[];
@@ -20,11 +22,26 @@ export interface CascaderOption extends Record<string, unknown> {
 
 let uid = 0;
 
+/** 计算当前的节点路径集 */
+const calculatePathNodes = (node: Node) => {
+  const nodes = [node];
+  let { parent } = node;
+
+  while (parent) {
+    // “先进后出” —— 每次循环，将层级小的节点推入到集合头部
+    nodes.unshift(parent);
+    parent = parent.parent;
+  }
+
+  return nodes;
+};
+
 class Node {
   readonly uid: number = uid++;
   readonly level: number;
   readonly label: string;
   readonly value: CascaderNodeValue;
+  readonly pathNodes: Node[];
 
   childrenData: CascaderOption[];
   children: Node[];
@@ -34,23 +51,43 @@ class Node {
   loaded: boolean;
 
   constructor(
-    readonly data: CascaderOption,
+    readonly data: Nullable<CascaderOption>,
     readonly config?: CascaderConfig,
     readonly parent?: Node,
     readonly root = false
   ) {
+    const childrenData = data?.children!;
+
     this.level = root ? 0 : parent ? parent.level + 1 : 1;
-    this.label = data.label!;
-    this.value = data.value!;
+    this.label = data?.label!;
+    this.value = data?.value!;
 
     // 原数据结构
-    this.childrenData = data.children!;
-    this.children = (data.children || []).map(
+    this.childrenData = childrenData;
+    this.children = (childrenData || []).map(
       (child) => new Node(child, config, this)
     );
 
+    this.pathNodes = calculatePathNodes(this);
     // this.loaded = !config.lazy || this.isLeaf || !isEmpty(childrenData);
     this.loaded = true;
+  }
+
+  get isLeaf(): boolean {
+    const { data, childrenData } = this;
+    const isLeaf =
+      !(isArray(childrenData) && childrenData.length) || data?.leaf;
+
+    return !!isLeaf;
+  }
+
+  appendChild(childData: CascaderOption) {
+    // ...
+  }
+
+  doCheck(checked: boolean) {
+    if (this.checked === checked) return;
+    this.checked = checked;
   }
 }
 
--- src/components/global/cascader/src/node.vue
+++ src/components/global/cascader/src/node.vue
@@ -1,20 +1,99 @@
 <!-- Node -->
 
 <script lang="ts" setup>
+import { computed, inject } from 'vue';
+import { CASCADER_PANEL_INJECTION_KEY } from './types';
 import type { default as CascaderNode } from './node';
 
 interface Props {
   node: CascaderNode;
 }
-defineProps<Props>();
+const props = defineProps<Props>();
+
+// get Context
+const panel = inject(CASCADER_PANEL_INJECTION_KEY)!;
+
+const isLeaf = computed(() => props.node.isLeaf);
+const isChecked = computed(() => props.node.checked);
+const inExpandingPath = computed(() => isInPath(panel.expandingNode));
+
+const isInPath = (node: CascaderNode) => {
+  const { level, uid } = props.node;
+  console.log('🏄 # isInPath # expandingNode', node);
+  // 在记录的路径集查找，是否匹配的到当前的节点
+  return node?.pathNodes[level - 1]?.uid === uid;
+};
+
+// 由子节点接受交互事件，而后通知父节点更新状态
+// 将当前节点的 checked 设置为 true，而且要把之前选中过的节点 checked 设置为 false（只考虑单选）
+const doCheck = (checked: boolean) => {
+  const { node } = props;
+  if (checked === node.checked) return;
+  panel.handleCheckChange(node, checked);
+};
+const doExpand = () => {
+  if (inExpandingPath.value) return;
+  panel.expandNode(props.node);
+};
+
+const handleCheck = (checked: boolean) => {
+  doCheck(checked);
+};
+const handleExpand = () => {
+  // node.loaded ? doExpand() : doLoad();
+  doExpand();
+};
+
+const handleClick = () => {
+  const { node } = props;
+  console.log('🏄 # handleClick # node', node);
+
+  if (isLeaf.value) {
+    handleCheck(true);
+  } else {
+    handleExpand();
+  }
+};
 </script>
 
 <template>
-  <div class="y-node-content">
-    <span>{{ node.label }}</span>
+  <div
+    class="y-node-content"
+    :class="[
+      inExpandingPath ? 'in-active-path' : '',
+      isChecked ? 'is-active' : '',
+    ]"
+    @click="handleClick"
+  >
+    <span class="y-node-label">{{ node.label }}</span>
   </div>
 </template>
 
 <style lang="scss">
-//
+.y-node-content {
+  padding: 0 30px 0 20px;
+  display: flex;
+  align-items: center;
+  height: 34px;
+  line-height: 34px;
+  cursor: pointer;
+
+  &.is-active {
+    background: #f5f7fa;
+  }
+  &.is-active,
+  &.in-active-path {
+    color: #409eff;
+    font-weight: 700;
+  }
+
+  .y-node-label {
+    flex: 1;
+    text-align: left;
+    padding: 0 8px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
 </style>
