1419a03066482f6fe75a58741f3f1dee062bf55b
Yoyeo98137 <1695782408@qq.com>
2022-08-07 17:15:59 +0800

    coding Cascader 实例导出

--- src/components/global/cascader/index.ts
+++ src/components/global/cascader/index.ts
@@ -1,5 +1,8 @@
 import YeoCascader from './src/index.vue';
 
 export default YeoCascader;
+
 export * from './src/types';
 export * from './src/node';
+
+export type YeoCascaderInstance = InstanceType<typeof YeoCascader>;
